6f27170a2cbfcc3e8a09f9dca94b598405cb30ef
Gitter499 <rafayel.amirkhanyan@gmail.com>
2024-01-18 19:44:33 -0500

    fix(CI): Added BuildConstants

---
+++ src/main/java/org/robolancers321/BuildConstants.java
@@ -0,0 +1,19 @@
+package org.robolancers321;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "321-Crescendo-2024";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 4;
+  public static final String GIT_SHA = "890b1f13f053a4a882dc48c8de3887bfe2f9294f";
+  public static final String GIT_DATE = "2024-01-18 19:40:03 EST";
+  public static final String GIT_BRANCH = "dev";
+  public static final String BUILD_DATE = "2024-01-18 19:43:26 EST";
+  public static final long BUILD_UNIX_TIME = 1705625006821L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
