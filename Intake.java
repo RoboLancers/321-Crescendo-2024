79930588ea8415f5516d7272a8343d8b827d407f
Raphael Hoang <fluffier913@gmail.com>
2024-01-24 15:09:09 -0500

    Added intake

--- Intake.java
+++ Intake.java
@@ -2,12 +2,35 @@ package org.robolancers321.subsystems;
 
 public class Intake extends SubsystemBase{
 
-    public CANSparkMax intakeMotor;
-    
+  public CANSparkMax intakemotor;
+  SparkPIDController IntakePIDController;
 
-    public Intake () {
+  public int kport = 0;
+  public double power = 0;
+  public double kFF = 0;
 
-        this.intakeMotor = new intakeMotor(0, brushless)
+  public void intake() {
 
-    }
+    this.intakemotor = new CANSparkMax(kport, MotorType.kBrushless);
+    this.intakemotor.setSmartCurrentLimit(20);
+    SmartDashboard.putNumber("intake-kFF", kFF);
+  }
+
+  @Override
+  public void periodic() {
+
+    intakemotor.set(power);
+    double kFF = SmartDashboard.getNumber("kFF", 0);
+    IntakePIDController.setFF(kFF);
+  }
+
+  public Command on(double power) {
+
+    return runOnce(() -> intakemotor.set(power));
+  }
+
+  public Command off(double power) {
+
+    return runOnce(() -> intakemotor.set(0));
+  }
 }
