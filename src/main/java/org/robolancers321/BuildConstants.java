97ab51f790a6fd67687c02526f80a2836a7c72e3
Raphael Hoang <fluffier913@gmail.com>
2024-01-26 17:14:07 -0500

    Added FF to intake

--- src/main/java/org/robolancers321/BuildConstants.java
+++ src/main/java/org/robolancers321/BuildConstants.java
@@ -7,16 +7,9 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "321-Crescendo-2024";
   public static final String VERSION = "unspecified";
-<<<<<<< HEAD
   public static final int GIT_REVISION = 9;
   public static final String GIT_SHA = "d7f5a071320caae2dbf837f6d4b8063c990ef911";
   public static final String GIT_DATE = "2024-01-24 17:03:30 EST";
-=======
-
-  public static final int GIT_REVISION = 6;
-  public static final String GIT_SHA = "6377635d69d5713f8f8b5400fc7a136abe5c431e";
-  public static final String GIT_DATE = "2024-01-21 18:36:14 EST";
->>>>>>> 61e5548f8a28f491003a4d55329b23a68f4bb910
   public static final String GIT_BRANCH = "dev-intake";
   public static final String BUILD_DATE = "2024-01-24 17:22:28 EST";
   public static final long BUILD_UNIX_TIME = 1706134948601L;
