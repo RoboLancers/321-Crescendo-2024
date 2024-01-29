abf90d962cf4ef8c4b171c3c84593b5efacaea64
Raphael Hoang <fluffier913@gmail.com>
2024-01-29 08:43:02 -0500

    Did the todo's and made a minor change to commands.

--- src/main/java/org/robolancers321/subsystem/Sucker.java
+++ src/main/java/org/robolancers321/subsystem/Sucker.java
@@ -60,14 +60,26 @@ public class Sucker extends SubsystemBase {
   }
 
   private void configureMotor(){
+
+    this.motor.setInverted(kInvertMotor);
+    this.motor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    this.motor.setSmartCurrentLimit(kCurrentLimit);
+    this.motor.enableVoltageCompensation(12);
     // TODO: inversion, current limit, idle mode, voltage compensation
   }
 
   public void configureEncoder(){
+
+    this.encoder.setVelocityConversionFactor(1);
     // TODO: set velocity conversion factor to 1
   }
 
   public void configureController(){
+
+    this.controller.setP(kP);
+    this.controller.setI(kI);
+    this.controller.setD(kD);
+    this.controller.setFF(kFF);
     // TODO: set kP, kI, kD, kFF on controller
   }
 
@@ -85,24 +97,39 @@ public class Sucker extends SubsystemBase {
   }
   
   public void initTuning(){
+
+    SmartDashboard.putNumber("kP", SmartDashboard.getNumber("kP", kP));
+    SmartDashboard.putNumber("kI", SmartDashboard.getNumber("kI", kI));
+    SmartDashboard.putNumber("kD", SmartDashboard.getNumber("kD", kD));
+    SmartDashboard.putNumber("kFF", SmartDashboard.getNumber("kFF", kFF));
     // TODO: put default values for kP, kI, kD, kFF on SmartDashboard
   }
 
   public void tune(){
+
+    double PTuned = SmartDashboard.getNumber("kP", kP);
+    double ITuned = SmartDashboard.getNumber("kI", kI);
+    double DTuned = SmartDashboard.getNumber("kD", kD);
+    double FFTuned = SmartDashboard.getNumber("kFF", kFF);
+
+    this.controller.setP(PTuned);
+    this.controller.setI(ITuned);
+    this.controller.setD(DTuned);
+    this.controller.setFF(FFTuned);
     // TODO: read the values for kP, kI, kD, kFF from SmartDashboard and update your controller
   }
 
   public Command in() {
-    return run(() -> this.controller.setReference(kInRPM, ControlType.kVelocity));
+    return run(() -> this.controller.setReference(kInRPM, ControlType.kVelocity)).andThen(() -> this.controller.setReference(0, ControlType.kVelocity));
   }
 
   public Command out(double power) {
-    return run(() -> this.controller.setReference(kOutRPM, ControlType.kVelocity));
+    return run(() -> this.controller.setReference(kOutRPM, ControlType.kVelocity)).andThen(() -> this.controller.setReference(0, ControlType.kVelocity));
   }
 
-  public Command off() {
-    return runOnce(() -> this.controller.setReference(0.0, ControlType.kVelocity));
-  }
+  // public Command off() {
+  //   return runOnce(() -> this.controller.setReference(0.0, ControlType.kVelocity));
+  // }
 
   public Command tuneController(){
     initTuning();
