1ae8db633885f6789586a67d2df372b0c5fa074f
AT727 <atxponh@gmail.com>
2024-02-09 15:51:34 -0500

    WE HAVE FIRE - vincent z

--- src/main/java/org/robolancers321/subsystems/LED.java
+++ src/main/java/org/robolancers321/subsystems/LED.java
@@ -339,19 +339,22 @@ public class LED extends VirtualSubsystem {
 
   @Override
   public void periodic() {
-    Optional<Consumer<AddressableLEDBuffer>> newPattern = Optional.empty();
+    // Optional<Consumer<AddressableLEDBuffer>> newPattern = Optional.empty();
 
-    for (var signal : ledSignals) {
-      if (signal.condition.getAsBoolean()) {
-        newPattern = Optional.of(signal.pattern);
-        break;
-      }
-    }
+    // for (var signal : ledSignals) {
+    //   if (signal.condition.getAsBoolean()) {
+    //     newPattern = Optional.of(signal.pattern);
+    //     break;
+    //   }
+    // }
 
-    // newPattern.orElse(currPattern).accept(ledBuffer);
-    if (newPattern.isPresent()) currPattern = newPattern.get();
+    // // newPattern.orElse(currPattern).accept(ledBuffer);
+    // if (newPattern.isPresent()) currPattern = newPattern.get();
 
-    currPattern.accept(ledBuffer);
+    // currPattern.accept(ledBuffer);
+
+    // meteorRain(ledBuffer, 0.02);
+    fire(ledBuffer, kCooling, kSparking);
 
     ledStrip.setData(ledBuffer);
   }
