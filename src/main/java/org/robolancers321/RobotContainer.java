bc01e84b264cf2385b88b18b0ef02f979e54ed80
mattperls-code <mattperls.code@gmail.com>
2024-02-09 16:07:28 -0500

    prep for tuning

--- src/main/java/org/robolancers321/RobotContainer.java
+++ src/main/java/org/robolancers321/RobotContainer.java
@@ -3,19 +3,27 @@ package org.robolancers321;
 
 import org.robolancers321.subsystems.intake.Intake;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
   Intake intake;
 
+  XboxController controller;
+
   public RobotContainer() {
     this.intake = Intake.getInstance();
+    this.controller = new XboxController(1);
 
-    configureBindings();
+    this.configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    new Trigger(this.controller::getAButton).whileTrue(this.intake.sucker.tuneController());
+    new Trigger(this.controller::getBButton).whileTrue(this.intake.retractor.tuneControllers());
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
